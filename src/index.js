5881678c0cbf442bb23f70b898e84e74f5a90c88
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-02-01 14:10:05 -0300

    feat: verifiy if user already exists

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ const costumers = [];
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  const costumersAlreadyExists = costumers.some(
+    (costumer) => costumer.cpf === cpf
+  );
+
+  if (costumersAlreadyExists) {
+    return response.status(400).json({ error: 'Costumer already exists!' });
+  }
 
   costumers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
